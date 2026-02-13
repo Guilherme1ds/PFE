8f2daa2959d4c6e911462977fb1f6af913f77b3c
Guilherme1ds <bernardiguilherme24@gmail.com>
2026-02-13 13:31:10 -0300

    update

--- Revisão HTML e CSS/rev1.js
+++ Revisão HTML e CSS/rev1.js
@@ -2,9 +2,8 @@ function converter() {
     let celsius = Number(document.getElementById("celsiusInput").value);
 
 let farenheit = celsius * 1.8 + 32;
-alert (`A temperatura em Farenheit é: ${farenheit}°F`);
 let kelvin = celsius + 273.15
-alert (`A temperatura em Kelvin é: ${kelvin}°K`)
+alert (`A temperatura em Farenheit é: ${farenheit}°F\nA temperatura em Kelvin é: ${kelvin}°K`)
 
 if (farenheit > 80) {
     document.body.style.backgroundColor = "coral";
